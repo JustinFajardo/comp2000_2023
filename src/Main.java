eac70a1321e9b7da823270dfdeb07d906c1cb6eb
JustinFajardo46427295 <justin.fajardo@students.mq.edu.au>
2023-07-26 08:43:51 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        System.out.println("~~~~");
+        System.out.println("Red vs. Blue");
+        System.out.println("~~~~~");
     }
 }
 
